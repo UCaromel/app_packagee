c248c74ea1b2f84f10fc0614c5a1d814abb4b868
Ulysse CAROMEL <132669192+UCaromel@users.noreply.github.com>
2023-07-24 12:46:06 +0200

    maj

--- install-packages.R
+++ install-packages.R
@@ -1,9 +1,2 @@
-# install-packages.R
-install.packages("devtools")
-devtools::install_version("lubridate", version = "1.9.2")
-devtools::install_version("purrr", version = "1.0.1")
-devtools::install_version("jsonlite", version = "1.8.7")
-devtools::install_version("readr", version = "2.1.4")
-devtools::install_version("httr", version = "1.4.2")
-remotes::install_github("UCaromel/movaround_pkg")
-
+install.packages(c("lubridate","purrr","jsonlite","readr","httr","dplyr", "remotes"))
+remotes::install_github("UCaromel/telraamStats")
