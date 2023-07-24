1678fe0f655c136bd392e1b8a38c70fe2cafdcbb
Ulysse CAROMEL <132669192+UCaromel@users.noreply.github.com>
2023-07-24 12:02:49 +0200

    using remotes instead of devtools

--- install-packages.R
+++ install-packages.R
@@ -1,2 +1,2 @@
-install.packages(c("lubridate","purrr","jsonlite","readr","httr","dplyr", "devtools"))
-devtools::install_github("https://github.com/UCaromel/movaround_pkg")
+install.packages(c("lubridate","purrr","jsonlite","readr","httr","dplyr", "remotes"))
+remotes::install_github("https://github.com/UCaromel/movaround_pkg")
