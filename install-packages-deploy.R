006346da005cdfa73537bbe8fee5f3a3325b0914
UCaromel <caromelulysse@gmail.com>
2023-07-25 01:28:04 +0200

    versions

--- install-packages-deploy.R
+++ install-packages-deploy.R
@@ -1,3 +1,6 @@
-install.packages(c('tidyverse', 'shiny', 'httr', 'jsonlite', 'lubridate', 'cowplot', 'CPAT', 'synchrony', 'forecast', 'zoo', 'readr', 'mgcv', 'remotes'), repos='https://cran.rstudio.com/')
+install.packages(c('shiny', 'httr', 'jsonlite', 'lubridate', 'cowplot', 'CPAT', 'synchrony', 'forecast', 'zoo', 'readr', 'mgcv', 'remotes'), repos='https://cran.rstudio.com/')
+remotes::install_version('textshaping', '0.3.6')
+remotes::install_version('ragg', '1.2.5')
+remotes::install_version('tidyverse', '2.0.0')
 remotes::install_version('rsconnect', '0.8.29')
 remotes::install_github('UCaromel/telraamStats')
