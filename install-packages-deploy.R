01bbfdbb4b70df570c7cf28abec7037001e005b9
UCaromel <caromelulysse@gmail.com>
2023-07-25 12:13:09 +0200

    setup r dependencies

--- install-packages-deploy.R
+++ install-packages-deploy.R
@@ -1,5 +1,3 @@
 install.packages(c('tidyverse','shiny', 'httr', 'jsonlite', 'lubridate', 'cowplot', 'CPAT', 'synchrony', 'forecast', 'zoo', 'readr', 'mgcv', 'remotes'), repos='https://cran.rstudio.com/')
 remotes::install_version('rsconnect', '0.8.29')
-remotes::install_github('r-lib/textshaping')
-remotes::install_github('r-lib/ragg')
 remotes::install_github('UCaromel/telraamStats')
